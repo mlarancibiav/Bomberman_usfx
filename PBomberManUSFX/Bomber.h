6a57c2d7e52d68100967888f4193db23a14ccdd5
Carlos Walter <pacheco.carlos@usfx.bo>
2021-09-09 15:36:20 -0400

    primeras clases, propiedades y metodos

---
+++ PBomberManUSFX/Bomber.h
@@ -0,0 +1,16 @@
+#pragma once
+class Bomber
+{
+private:
+	int anchoAvatar;
+	int altoAvatar;
+
+public:
+	int getAnchoAvatar() { return anchoAvatar; }
+	void setAnchoAvatar(int _anchoAvatar) { anchoAvatar = _anchoAvatar;}
+
+	int getAltoAvatar() { return altoAvatar; }
+	void setAltoAvatar(int _altoAvatar) { altoAvatar = _altoAvatar; }
+
+};
+
