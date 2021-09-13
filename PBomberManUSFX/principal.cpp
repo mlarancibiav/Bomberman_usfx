6f5f6a3d16d333293a24372753d79c0945c9c577
Carlos Walter <pacheco.carlos@usfx.bo>
2021-09-13 13:02:42 -0400

    clase detonador

--- PBomberManUSFX/principal.cpp
+++ PBomberManUSFX/principal.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include "Detonador.h"
+
 using namespace std;
 
 int main() {
 
 	cout << "Primer paso rumbo a BomberMan" << endl;
+
+	Detonador d1;
+
+	d1.setHoras(5);
+	d1.setMinutos(20);
+	d1.setSegundos(10);
+
+	while (d1.getEjecutado() != true) {
+		d1.ReducirTiempo();
+	}
+
+	d1.Explotar();
+
 
 	return 0;
 }
